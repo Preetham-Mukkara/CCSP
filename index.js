22c752ac1aa2e130ed7ea1eaca967cfe28ff5ebb
Preetham Mukkara <preetham@lastlock.com>
2023-02-03 13:54:47 -0600

    extracting data from pdf as desired

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const pdf = require('pdf-parse');
 const prompt = require('prompt-sync')({sigint: true});
 const fs = require('fs');
+var transactions = Array()
+var dates = Array()
+var locations = Array()
+var costs = Array()
 let options = {
     pagerender: render_page,
     max: 0,
@@ -16,22 +20,52 @@ function getFilename(){
         console.log("WOOOHOOO");
         let dataBuffer = fs.readFileSync(filePath);
         pdf(dataBuffer,options).then(function(data){
-            console.log(data.text);
+            //console.log(data)
+            parseData(data.text)
         }).catch(function(error){
             console.log(error);
         });
     }
 }
 
+function findIndex(str){
+    var regex = /^[a-zA-Z]+$/
+    let index = -1
+    for(let i =0; i < str.length;i++){
+        if(str.charAt(i).match(regex)){
+            index = i;
+        }
+    }
+    return index
+}
+
+function parseData(data){
+    var lines = data.split("\n");
+    for(let i =0; i < lines.length; i++){
+        var line = lines[i];
+        if(line.indexOf('/') == 2 && line.indexOf(' ') == 5){
+            transactions.push(line)
+        }
+    }
+    for(let i =0; i<transactions.length;i++){
+        var index = findIndex(transactions[i])
+        dates.push(transactions[i].slice(0,5).trim())
+        locations.push(transactions[i].slice(5,index+1).trim())
+        costs.push(transactions[i].slice(index+1).trim())
+    }
+    console.log(dates)
+    console.log(locations)
+    console.log(costs)
+}
+
 
 function render_page(pageData){
     let render_options = {
         //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
-        normalizeWhitespace: false,
+        normalizeWhitespace: true,
         //do not attempt to combine same line TextItem's. The default value is `false`.
         disableCombineTextItems: false
     }
- 
     return pageData.getTextContent(render_options)
     .then(function(textContent) {
         let lastY, text = '';
